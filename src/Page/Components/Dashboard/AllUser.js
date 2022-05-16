6f89ba81e57307222afe4e7f6ed8c467c29c5856
its-kayes <kayes.ek8@gmail.com>
2022-05-17 04:37:07 +0600

    Show All user with JWT

--- src/Page/Components/Dashboard/AllUser.js
+++ src/Page/Components/Dashboard/AllUser.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useQuery } from 'react-query';
+import { toast } from 'react-toastify';
 import Loading from '../../Shared/Loading';
 
 const AllUser = () => {
@@ -11,9 +12,30 @@ const AllUser = () => {
     //         .then(data => setUsers(data))
     // }, []);
 
-    let {data: users, isLoading} = useQuery('user', () => fetch('http://localhost:5000/user').then(res => res.json()))
+    let { data: users, isLoading, refetch } = useQuery('user', () => fetch('http://localhost:5000/user', {
+        method: 'GET',
+        headers: {
+            // 'content-type': 'application/json',
+            authorization: `Bearer ${localStorage.getItem('accessToken')}`
+        },
+    }).then(res => res.json()))
 
-    if(isLoading) {
+    let makeAdmin = (email) => {
+        fetch(`http://localhost:5000/user/admin/${email}`, {
+            method: 'PUT',
+            headers: {
+                authorization: `Bearer ${localStorage.getItem('accessToken')}`
+            },
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                refetch();
+                toast.success(`Successfully Added as an Admin`);
+            })
+    }
+
+    if (isLoading) {
         return <Loading> </Loading>
     }
 
@@ -28,20 +50,25 @@ const AllUser = () => {
                         <tr>
                             <th></th>
                             <th>Email Address</th>
-                            {/* <th>Service</th>
-                            <th>Time</th>
-                            <th>Date</th> */}
+                            <th>Make Role</th>
+                            <th>Delete User</th>
+                            {/* <th>Date</th> */}
                         </tr>
                     </thead>
                     {
                         users.map((user, index) => <tbody key={user._id}>
-                            
+
                             <tr>
                                 <th> {index + 1} </th>
                                 <td> {user.email} </td>
-                                {/* <td> {appointment.service} </td>
-                                <td> {appointment.slot} </td>
-                                <td> {appointment.date} </td> */}
+                                <td>
+                                    {
+                                        user.role !== 'admin' && <button onClick={() => makeAdmin(user.email)} class="btn btn-sm">Make Admin</button>
+                                    }
+                                </td>
+                                <td> <button class="btn btn-sm">Remove User</button>
+                                </td>
+                                {/* <td> {appointment.date} </td> */}
                             </tr>
                         </tbody>)
                     }
