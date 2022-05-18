b9e1b0d6414711901ad10c4f4ef6f36aa8dc8359
its-kayes <kayes.ek8@gmail.com>
2022-05-18 16:07:40 +0600

    get Upload Img Link

--- src/Page/Components/Dashboard/AddDoctor.js
+++ src/Page/Components/Dashboard/AddDoctor.js
@@ -1,9 +1,10 @@
+import axios from 'axios';
 import React, { useState } from 'react';
 import { useQuery } from 'react-query';
 import Loading from '../../Shared/Loading';
 
 const AddDoctor = () => {
-    // let [services, setServices] = useState();
+    let [imageUrl, setImageUrl] = useState();
 
     let { data: services, isLoading } = useQuery('service', () => fetch('http://localhost:5000/services').then(res => res.json()))
 
@@ -17,17 +18,66 @@ const AddDoctor = () => {
         let email = event.target.email.value;
         let name = event.target.name.value;
         let specialist = event.target.specialist.value;
-        let image = event.target.image.value;
+        // let image = event.target.image.value;
 
         let data = {
             name: name,
             email: email,
             specialist: specialist,
-            image: image
+            image: imageUrl
         }
+
+        // console.log(imageUrl);
         console.log(data);
         event.target.reset();
     }
+
+
+
+    // this code written hy myself 
+    let handleImageUpload = image => {
+        // console.log(image.target.files[0]);
+        const imageData = new FormData();
+        let imgSecterKey = 'c25b9d9e071325c2967df21a4422b129';
+        let url = `https://api.imgbb.com/1/upload?key=${imgSecterKey}`
+        imageData.append('image', image.target.files[0]);
+
+        fetch(url, {
+            method: 'POST',
+            body: imageData
+        })
+            .then(response => {
+                return response.json()
+            })
+            .then(result => {
+                // console.log('Success:', result);
+                // console.log(result.data.image.url);
+                setImageUrl(result.data.image.url);
+            })
+    }
+
+
+
+
+    // This Code Given By Support Session 
+
+
+    // const handleImageUpload = image => {
+    //     console.log(image.target.files[0]);
+    //     const imageData = new FormData();
+    //     imageData.set('key', 'c25b9d9e071325c2967df21a4422b129');
+    //     imageData.append('image', image.target.files[0]);
+
+    //     axios.post('https://api.imgbb.com/1/upload', imageData)
+    //         .then(function (response) {
+    //             console.log(response.data.data.display_url);
+
+    //         })
+    //         .catch(function (error) {
+    //             console.log(error);
+    //         });
+    // }
+
 
     return (
         <div>
@@ -52,8 +102,16 @@ const AddDoctor = () => {
                                     }
                                 </select>
 
-                                <label htmlFor="image" className='my-4'> Image </label>
-                                <input name='image' type="file" placeholder="image" className=" input-bordered w-full max-w-xs" required />
+                                {/* <label htmlFor="image" className='my-4'> Image </label>
+                                <input name='image' type="file" placeholder="image" className=" input-bordered w-full max-w-xs" required /> */}
+
+
+                                <div className="custom-file">
+                                    <input type="file" className="custom-file-input" style={{ cursor: 'pointer' }} id="customFile" name="file" onChange={handleImageUpload} required />
+                                    <label className="custom-file-label" htmlFor="customFile">Upload an Image</label>
+                                </div>
+
+
 
                                 <input type='submit' value='Add' className="mt-2 btn btn-dark w-full max-w-sm" />
                             </form>
